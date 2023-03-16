7fe705eec1548b415febad4bc9882ce94ab862e9
kaewdungdee2538 <kaewdungdee2538@gmail.com>
2023-03-16 19:25:11 +0700

    create function numeric and validation

--- time/time.go
+++ time/time.go
@@ -151,3 +151,20 @@ func CalParkingInterval(parkingInTime string, parkingOutTime string) string {
 	minutesTimeDiff := CalculateBetweenTimeInAndTimeOutToDuration(parkingTimeIn, parkingTimeOut)
 	return ConverDurationToText(minutesTimeDiff)
 }
+
+func ConvertStringDatetime(str string) string {
+
+	layout := "2006-01-02T15:04:05.000000"
+	t, err := time.Parse(layout, str)
+	if err != nil {
+		layout2 := "2006-01-02T15:04:05.000Z"
+		t2, err := time.Parse(layout2, str)
+		if err != nil {
+			fmt.Println(err)
+			return str
+		}
+		return t2.Format("2006-01-02 15:04:05")
+	}
+	timeText := t.Format("2006-01-02 15:04:05")
+	return timeText
+}
