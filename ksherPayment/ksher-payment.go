d79be8192d6f7761589ae0bb47c776485e936daf
kaewdungdee2538 <kaewdungdee2538@gmail.com>
2023-05-25 13:35:09 +0700

    create function verifySign

--- ksherPayment/ksher-payment.go
+++ ksherPayment/ksher-payment.go
@@ -8,3 +8,12 @@ func GenerateSign(privateKeyPath string) (string, error) {
 	privateKeyData := KsherGO.ReadPrivateKeyFromPath(privateKeyPath)
 	return KsherGO.KsherSign(nil, privateKeyData)
 }
+
+func VerifySig(response KsherGO.KsherResp) error {
+	publicKey := []byte(`
+-----BEGIN PUBLIC KEY-----
+MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAL7955OCuN4I8eYNL/mixZWIXIgCvIVE
+ivlxqdpiHPcOLdQ2RPSx/pORpsUu/E9wz0mYS2PY7hNc2mBgBOQT+wUCAwEAAQ==
+-----END PUBLIC KEY-----`)
+	return KsherGO.KsherVerify(response, publicKey)
+}
