ceb9a744e4784092c045639c8db339ddb3807399
kaewdungdee2538 <kaewdungdee2538@gmail.com>
2023-05-26 10:19:14 +0700

    create function calculate total fee to ksher fee and get ksher msg from error code

--- ksherPayment/ksher-payment.go
+++ ksherPayment/ksher-payment.go
@@ -1,6 +1,8 @@
 package ksherPayment
 
 import (
+	"strings"
+
 	"github.com/ksher-api/ksher-sdk/go/src/KsherGO"
 )
 
@@ -16,4 +18,20 @@ MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAL7955OCuN4I8eYNL/mixZWIXIgCvIVE
 ivlxqdpiHPcOLdQ2RPSx/pORpsUu/E9wz0mYS2PY7hNc2mBgBOQT+wUCAwEAAQ==
 -----END PUBLIC KEY-----`)
 	return KsherGO.KsherVerify(response, publicKey)
+}
+
+func CalculateTotalFeeToKsherFee(totalFee float64) int {
+	total := totalFee * 100
+	return int(total)
+}
+
+func GetKsherMsgFromErrorCode(errCode string, errMsg string) string {
+	errCodeUpperCase := strings.ToUpper(errCode)
+	switch errCodeUpperCase {
+	case "KSHER_ORDER_PAID":
+		return "รายการซ้ำในระบบ"
+	default:
+		return errMsg
+	}
+
 }
