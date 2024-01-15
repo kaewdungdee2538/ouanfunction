8e649f16076b6dd5df39e742fc68444c0e42d431
kaewdungdee2538 <kaewdungdee2538@gmail.com>
2024-01-15 09:36:18 +0700

    create function for encode and decode base64 module

--- base64/base64.go
+++ base64/base64.go
@@ -7,3 +7,15 @@ import (
 func ByteToBase64String(b []byte) string {
 	return base64.StdEncoding.EncodeToString(b)
 }
+
+func Encode(plaintext string) string {
+	return base64.StdEncoding.EncodeToString([]byte(plaintext))
+}
+
+func Decode(encrypt string) (string, error) {
+	data, err := base64.StdEncoding.DecodeString(encrypt)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
