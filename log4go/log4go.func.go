40a0ebe2610a7b9faf4cd81f22dda159993e6b0a
kaewdungdee2538 <kaewdungdee2538@gmail.com>
2024-04-09 17:41:15 +0700

    upgrade: upgrade to version2

--- log4go/log4go.func.go
+++ log4go/log4go.func.go
@@ -2,224 +2,96 @@ package log4go
 
 import (
 	"fmt"
-	"net"
+	"log"
+	"os"
 	"time"
 
 	"github.com/alecthomas/log4go"
-	"github.com/kaewdungdee2538/ouanfunction/directoryfunc"
-	"github.com/kaewdungdee2538/ouanfunction/formatString"
-	"github.com/kaewdungdee2538/ouanfunction/json"
+	"github.com/kaewdungdee2538/ouanfunction/v2/directoryfunc"
+	"github.com/kaewdungdee2538/ouanfunction/v2/formatString"
+	"github.com/kaewdungdee2538/ouanfunction/v2/json"
 )
 
-// ---------------------------------------------------------------------------------------------------------------//
-func WriteLogDebugAll(model Log4goModel, logMode string) {
-
-	log := log4go.NewDefaultLogger(log4go.DEBUG)
-
-	defer log.Close()
-	// check directory
-	currentDirectory := fmt.Sprintf(`%s/debug`, model.Driectory)
-	directoryfunc.CheckDirectory(currentDirectory)
-
-	// generate file log writer formater
-	flw := generateFormatLog(currentDirectory, model.AppName)
-
-	log.AddFilter("log", log4go.DEBUG, flw)
-	logInfo := createLogFormat(model)
-
+// ----------------- verson2
+func (l Log4goLogger) WriteLog(model Log4goModel, logMode string) {
 	switch logMode {
 	case "warning":
-		log.Warn(logInfo)
-		go WriteLogWarning(model)
+		go l.writeNewLogWarning(model)
 	case "error":
-		log.Error(logInfo)
-		go WriteLogError(model)
+		go l.writeNewLogError(model)
 	case "critical":
-		log.Critical(logInfo)
-		go WriteLogCritical(model)
+		go l.writeNewLogCritical(model)
 	default:
-		log.Info(logInfo)
-		go WriteLogInfo(model)
+		go l.writeNewLogInfo(model)
 	}
 }
 
 // info log
-// ---------------------------------------------------------------------------------------------------------------//
-func WriteLogInfo(model Log4goModel) {
+func (l Log4goLogger) writeNewLogInfo(model Log4goModel) {
 	log := log4go.NewDefaultLogger(log4go.INFO)
 	defer log.Close()
 	// check directory
-	currentDirectory := fmt.Sprintf(`%s/info`, model.Driectory)
+	currentDirectory := model.Driectory
 	directoryfunc.CheckDirectory(currentDirectory)
 
 	// generate file log writer formater
-	flw := generateFormatLog(currentDirectory, model.AppName)
+	flw := l.generateFormatLog(currentDirectory, model.AppName)
 
 	log.AddFilter("log", log4go.INFO, flw)
-	logInfo := createLogFormat(model)
+	logInfo := l.newCreateLogFormat(model)
 	log.Info(logInfo)
 }
 
 // warning log
-func WriteLogWarning(model Log4goModel) {
+func (l Log4goLogger) writeNewLogWarning(model Log4goModel) {
 	log := log4go.NewDefaultLogger(log4go.WARNING)
 	defer log.Close()
 	// check directory
-	currentDirectory := fmt.Sprintf(`%s/warning`, model.Driectory)
+	currentDirectory := model.Driectory
 	directoryfunc.CheckDirectory(currentDirectory)
 
 	// generate file log writer formater
-	flw := generateFormatLog(currentDirectory, model.AppName)
+	flw := l.generateFormatLog(currentDirectory, model.AppName)
 
 	log.AddFilter("log", log4go.WARNING, flw)
-	logInfo := createLogFormat(model)
+	logInfo := l.newCreateLogFormat(model)
 	log.Warn(logInfo)
 }
 
 // error log
-func WriteLogError(model Log4goModel) {
+func (l Log4goLogger) writeNewLogError(model Log4goModel) {
 	log := log4go.NewDefaultLogger(log4go.ERROR)
 	defer log.Close()
 	// check directory
-	currentDirectory := fmt.Sprintf(`%s/error`, model.Driectory)
+	currentDirectory := model.Driectory
 	directoryfunc.CheckDirectory(currentDirectory)
 
 	// generate file log writer formater
-	flw := generateFormatLog(currentDirectory, model.AppName)
+	flw := l.generateFormatLog(currentDirectory, model.AppName)
 
 	log.AddFilter("log", log4go.ERROR, flw)
-	logInfo := createLogFormat(model)
+	logInfo := l.newCreateLogFormat(model)
 	log.Error(logInfo)
 }
 
 // critical log
-func WriteLogCritical(model Log4goModel) {
+func (l Log4goLogger) writeNewLogCritical(model Log4goModel) {
 	log := log4go.NewDefaultLogger(log4go.CRITICAL)
 	defer log.Close()
 	// check directory
-	currentDirectory := fmt.Sprintf(`%s/critical`, model.Driectory)
+	currentDirectory := model.Driectory
 	directoryfunc.CheckDirectory(currentDirectory)
 
 	// generate file log writer formater
-	flw := generateFormatLog(currentDirectory, model.AppName)
+	flw := l.generateFormatLog(currentDirectory, model.AppName)
 
 	log.AddFilter("log", log4go.CRITICAL, flw)
-	logInfo := createLogFormat(model)
+	logInfo := l.newCreateLogFormat(model)
 	log.Critical(logInfo)
 }
 
-// for *gin.Context
-func createLogFormat(model Log4goModel) string {
-	dataJsonText, _ := json.PrettyStructJson(model.Data)
-
-	var clientAddress = ""
-	var remotePort = ""
-	var fullPath = ""
-	if model.Context != nil {
-		if ip, port, err := net.SplitHostPort(model.Context.Request.RemoteAddr); err == nil {
-			clientAddress = ip
-			remotePort = port
-			fullPath = model.Context.FullPath()
-		} else {
-			clientAddress = model.Context.ClientIP()
-			fullPath = model.Context.FullPath()
-		}
-	}
-
-	logInfo := fmt.Sprintf(`
-	APP_NAME=> %s
-	FUNCTION_NAME => [%s] 
-	REMOTE_ADDRESS => %s
-	REMOTE_PORT => %s 
-	ORIGINALURL => %s
-	NODE_ID => %s
-	MESSAGE => [%s]
-	DATA => %v
-	`,
-		model.AppName, model.FunctionName, clientAddress, remotePort, fullPath, model.NodeId, model.Msg, dataJsonText,
-	)
-	return logInfo
-}
-
-
-//----------------- new verson
-func WriteNewLogDebugAll(model Log4goModel, logMode string) {
-	switch logMode {
-	case "warning":
-		go WriteNewLogWarning(model)
-	case "error":
-		go WriteNewLogError(model)
-	case "critical":
-		go WriteNewLogCritical(model)
-	default:
-		go WriteNewLogInfo(model)
-	}
-}
-// info log
-func WriteNewLogInfo(model Log4goModel) {
-	log := log4go.NewDefaultLogger(log4go.INFO)
-	defer log.Close()
-	// check directory
-	currentDirectory := fmt.Sprintf(`%s`, model.Driectory)
-	directoryfunc.CheckDirectory(currentDirectory)
-
-	// generate file log writer formater
-	flw := generateFormatLog(currentDirectory, model.AppName)
-
-	log.AddFilter("log", log4go.INFO, flw)
-	logInfo := newCreateLogFormat(model)
-	log.Info(logInfo)
-}
-
-// warning log
-func WriteNewLogWarning(model Log4goModel) {
-	log := log4go.NewDefaultLogger(log4go.WARNING)
-	defer log.Close()
-	// check directory
-	currentDirectory := fmt.Sprintf(`%s`, model.Driectory)
-	directoryfunc.CheckDirectory(currentDirectory)
-
-	// generate file log writer formater
-	flw := generateFormatLog(currentDirectory, model.AppName)
-
-	log.AddFilter("log", log4go.WARNING, flw)
-	logInfo := newCreateLogFormat(model)
-	log.Warn(logInfo)
-}
-
-// error log
-func WriteNewLogError(model Log4goModel) {
-	log := log4go.NewDefaultLogger(log4go.ERROR)
-	defer log.Close()
-	// check directory
-	currentDirectory := fmt.Sprintf(`%s`, model.Driectory)
-	directoryfunc.CheckDirectory(currentDirectory)
-
-	// generate file log writer formater
-	flw := generateFormatLog(currentDirectory, model.AppName)
-
-	log.AddFilter("log", log4go.ERROR, flw)
-	logInfo := newCreateLogFormat(model)
-	log.Error(logInfo)
-}
-
-// critical log
-func WriteNewLogCritical(model Log4goModel) {
-	log := log4go.NewDefaultLogger(log4go.CRITICAL)
-	defer log.Close()
-	// check directory
-	currentDirectory := fmt.Sprintf(`%s`, model.Driectory)
-	directoryfunc.CheckDirectory(currentDirectory)
-
-	// generate file log writer formater
-	flw := generateFormatLog(currentDirectory, model.AppName)
-
-	log.AddFilter("log", log4go.CRITICAL, flw)
-	logInfo := newCreateLogFormat(model)
-	log.Critical(logInfo)
-}
-// for 
-func newCreateLogFormat(model Log4goModel) string {
+// for
+func (l Log4goLogger) newCreateLogFormat(model Log4goModel) string {
 	dataJsonText, _ := json.PrettyStructJson(model.Data)
 
 	logInfo := fmt.Sprintf(`
@@ -237,18 +109,47 @@ func newCreateLogFormat(model Log4goModel) string {
 	return logInfo
 }
 
-// ---------------------------------------------------------------------------------------------------------------//
-func getCurrentDate(appName string) string {
+// get current date
+func (l Log4goLogger) getCurrentDate(appName string) string {
 	appname := formatString.StringToCamelCase(appName)
 	now := time.Now()
 	currentdate_str := fmt.Sprintf("%s.%s", appname, now.Format("2006-01-02"))
 	return currentdate_str
 }
 
-// ---------------------------------------------------------------------------------------------------------------//
-func generateFormatLog(currentDirectory string, appName string) *log4go.FileLogWriter {
-	logFileName := fmt.Sprintf(`%s/%s.log`, currentDirectory, getCurrentDate(appName))
+// get older date
+func (l Log4goLogger) getOlderDate(appName string) string {
+	maxDays := 30
+	if l.MaxDaysBackup > 0 {
+		maxDays = l.MaxDaysBackup
+	}
+	appname := formatString.StringToCamelCase(appName)
+	now := time.Now().Add(time.Duration(-maxDays) * (time.Hour * 24))
+	currentdate_str := fmt.Sprintf("%s.%s", appname, now.Format("2006-01-02"))
+	return currentdate_str
+}
+
+// generate format
+func (l Log4goLogger) generateFormatLog(currentDirectory string, appName string) *log4go.FileLogWriter {
+	// delete old log
+	go l.deleteOldLogFile(currentDirectory, appName)
+	logFileName := fmt.Sprintf(`%s/%s.log`, currentDirectory, l.getCurrentDate(appName))
 	flw := log4go.NewFileLogWriter(logFileName, false)
 	flw.SetFormat("[%D %T] [%L], %M")
 	return flw
+}
+
+// delete older log file
+func (l Log4goLogger) deleteOldLogFile(currentDirectory string, appName string) *log4go.FileLogWriter {
+	// get old date
+	l.getOlderDate(appName)
+	logFileName := fmt.Sprintf(`%s/%s.log`, currentDirectory, l.getOlderDate(appName))
+	if _, err := os.Stat(logFileName); err == nil {
+		e := os.Remove(logFileName)
+		if e != nil {
+			log.Println(e)
+		}
+		log.Println("delete older log file : ", logFileName)
+	}
+	return nil
 }
