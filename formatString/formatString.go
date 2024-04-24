0845bc95b4eb60d559cc868bf6ba2b70badb7dce
kaewdungdee2538 <kaewdungdee2538@gmail.com>
2024-04-24 10:36:38 +0700

    feat: create funtion ConvertArrayToString

--- formatString/formatString.go
+++ formatString/formatString.go
@@ -51,3 +51,10 @@ func SetValueForWhereLikeData(input string) string {
 	result := fmt.Sprintf("%s%s%s", likeChar, input, likeChar)
 	return result
 }
+
+
+func ConvertArrayToString(a []int, delim string) string {
+    return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
+    //return strings.Trim(strings.Join(strings.Split(fmt.Sprint(a), " "), delim), "[]")
+    //return strings.Trim(strings.Join(strings.Fields(fmt.Sprint(a)), delim), "[]")
+}
